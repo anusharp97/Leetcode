3b396f56438715150d0ec85eac0554c1a0f874a3
anusharp97 <35512779+anusharp97@users.noreply.github.com>
2020-07-26 20:15:50 +0530

    Create addDigits.py

---
+++ addDigits.py
@@ -0,0 +1,17 @@
+'''
+258. Add Digits
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+             
+Refrenece: https://en.wikipedia.org/wiki/Digital_root
+'''
+
+def addDigits(self, num: int) -> int:
+  if num <10:
+      return num
+  return (1+((num-1)%9))
