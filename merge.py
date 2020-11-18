754d28ef8816cc222e7d537f3cdf7b8df5c5da41
anusharp97 <35512779+anusharp97@users.noreply.github.com>
2020-11-18 16:32:37 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -14,31 +14,50 @@ Output: [[1,5]]
 Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 '''
 def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-  if not intervals:
-      return
-  intervals.sort(key = lambda x: x[0])
+  intervals.sort(key=lambda x:x[0])
   # sort the intervals by their start value, then each set of intervals that can be merged will appear as a contiguous "run" in the sorted list.
+  res = []
   n = len(intervals)
-  prevStart, prevEnd = intervals[0][0], intervals[0][1]
-  res = {}
-  res[prevStart] = prevEnd
+  if n<=1:
+      return intervals
+  start = intervals[0][0]
+  end = intervals[0][1]
   for i in range(1,n):
-      curStart, curEnd = intervals[i][0], intervals[i][1]
-      # if the current interval begins before the end of previous interval
-      # we merge them by updating the end of the previous interval if it is less than the end of the current interval.
-      if prevEnd >= curStart:
-          if curEnd > prevEnd:
-              res[prevStart] = curEnd
-              prevEnd = curEnd
+      curStart = intervals[i][0]
+      curEnd = intervals[i][1]
+      if curStart<=end:
+          end = max(curEnd,end)
       else:
-      # If the current interval begins after the previous interval ends, then they do not overlap
-          res[curStart] = curEnd
-          prevStart = curStart
-          prevEnd = curEnd
-  result = []
-  for key,val in res.items():
-      result.append([key,val])
-  return result
+          res.append([start,end])
+          start = curStart
+          end = curEnd
+  if [start,end] not in res:
+      res.append([start,end])
+  return res
+  #   if not intervals:
+#       return
+#   intervals.sort(key = lambda x: x[0])
+#   n = len(intervals)
+#   prevStart, prevEnd = intervals[0][0], intervals[0][1]
+#   res = {}
+#   res[prevStart] = prevEnd
+#   for i in range(1,n):
+#       curStart, curEnd = intervals[i][0], intervals[i][1]
+#       # if the current interval begins before the end of previous interval
+#       # we merge them by updating the end of the previous interval if it is less than the end of the current interval.
+#       if prevEnd >= curStart:
+#           if curEnd > prevEnd:
+#               res[prevStart] = curEnd
+#               prevEnd = curEnd
+#       else:
+#       # If the current interval begins after the previous interval ends, then they do not overlap
+#           res[curStart] = curEnd
+#           prevStart = curStart
+#           prevEnd = curEnd
+#   result = []
+#   for key,val in res.items():
+#       result.append([key,val])
+#   return result
   
   '''
   Time complexity = O(nlogn)
